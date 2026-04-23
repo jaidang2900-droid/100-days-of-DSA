c35e9db073d36f80c76e23f26bebe16766bf5d66
jaidang2900-droid <jaidang2900@gmail.com>
2026-04-23 19:24:45 +0530

    Add files via upload

---
+++ Day_81/Q-1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        // Optimization: check if swapping happens
+        int swapped = 0;
+
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = 1;
+            }
+        }
+
+        // If no two elements were swapped, array is sorted
+        if (swapped == 0)
+            break;
+    }
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
